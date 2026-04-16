3bec691aa5360d2f3bffb79b9266fe7ddfbdddee
jiabw.cn <jiabw.cn@gmail.com>
2026-04-16 14:20:27 +0800

    update codecompanion

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -1,21 +1,19 @@
 return {
   "olimorris/codecompanion.nvim",
-  version = "^19.0.0",
   dependencies = {
     "nvim-lua/plenary.nvim",
     "nvim-treesitter/nvim-treesitter",
   },
-  opts = {
-    interactions = {
-      chat = {
-        adapter = "anthropic",
-        model = "claude-sonnet-4-20250514"
-      },
-    },
-    opts = {
-      log_level = "DEBUG",
-    },
-  },
   config = function()
+    require("codecompanion").setup{
+      strategies = {
+        chat = {
+          adapter = 'gemini',
+        },
+        inline = {
+          adapter = 'gemini',
+        },
+      }
+    }
   end
 }
