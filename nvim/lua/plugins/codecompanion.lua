b92b2ea0d645cd49ecdf15398cf4b2027753eaa4
jiabw.cn <jiabw.cn@gmail.com>
2026-05-06 20:14:17 +0800

    update codecompanion

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -10,42 +10,41 @@ return {
         http = {
           anthropic = function()
             return require("codecompanion.adapters").extend("anthropic", {
-              url = "https://tokenverse.corp.kuaishou.com/v1",
+              url = "https://tokenverse.corp.kuaishou.com/v1/chat/completions",
               env = {
-                -- api_key = "sk-2PoySFmky9pHUotjb",
                 api_key = function()
                   return os.getenv("TOKENVERSE_API_KEY")
                 end
               },
               headers = {
-                -- ["Authorization"] = "Bearer ${api_key}",
+                ["Authorization"] = "Bearer ${api_key}",
+                ["Content-Type"] = "application/json",
                 ["x-api-key"] = "${api_key}",
               },
             })
           end,
         },
       },
-      interactions = {
-        chat = {
-          adapter = "anthropic",
-          model = "tokenverse/GLM-5"
-        },
-        background = {
-          adapter = {
-            name = "anthropic",
-            model = "tokenverse/GLM-5",
-          }
-        },
-        inline = {
-          adapter = "anthropic",
-        },
-        cmd = {
-          adapter = "anthropic",
-        },
-      },
       opts = {
-        log_level = "DEBUG",
         language = "Chinese",
+        interactions = {
+          chat = {
+            adapter = "anthropic",
+            model = "GLM-5.1"
+          },
+          background = {
+            adapter = {
+              name = "anthropic",
+              model = "GLM-5.1"
+            }
+          },
+          inline = {
+            adapter = "anthropic",
+          },
+          cmd = {
+            adapter = "anthropic",
+          },
+        },
       }
     })
   end
