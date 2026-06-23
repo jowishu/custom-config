529bf85225afd4362a05f999bb19fc13da7c4801
jiabw.cn <jiabw.cn@gmail.com>
2026-06-23 15:54:30 +0800

    update markdown render

--- nvim/lua/plugins/md-render.lua
+++ nvim/lua/plugins/md-render.lua
@@ -1,22 +1,42 @@
 return {
-  "OXY2DEV/markview.nvim",
+  "MeanderingProgrammer/render-markdown.nvim",
   dependencies = {
-    "nvim-treesitter/nvim-treesitter",
-    "nvim-tree/nvim-web-devicons"
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-tree/nvim-web-devicons',
   },
   config = function()
-    require("markview").setup({
-      modes = { "n", "i", "no", "c" },
-      hybrid_modes = { "i" },
-      preview = {
-        enable = false,
-      },
-      callbacks = {
-        on_enable = function(_, win)
-          vim.wo[win].conceallevel = 2
-          vim.wo[win].concealcursor = "nc"
-        end,
-      },
+    require('render-markdown').setup({
+      render_modes = { 'n', 'c', 't' },
+      preset = 'none',
+      file_types = { 'markdown' },
+      latex = {
+        enabled = true,
+        converter = { 'utftex', 'latex2text' },
+        highlight = 'RenderMarkdownMath',
+        position = 'center',
+      }
     })
   end
 }
+-- return {
+--   "OXY2DEV/markview.nvim",
+--   dependencies = {
+--     "nvim-treesitter/nvim-treesitter",
+--     "nvim-tree/nvim-web-devicons"
+--   },
+--   config = function()
+--     require("markview").setup({
+--       modes = { "n", "i", "no", "c" },
+--       hybrid_modes = { "i" },
+--       preview = {
+--         enable = false,
+--       },
+--       callbacks = {
+--         on_enable = function(_, win)
+--           vim.wo[win].conceallevel = 2
+--           vim.wo[win].concealcursor = "nc"
+--         end,
+--       },
+--     })
+--   end
+-- }
